e5cd264a99da75b97a608d26044e5521e37c29d7
Nicholas Johnson <nyck.johnson@gmail.com>
2015-12-10 14:25:32 -0800

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -32,7 +32,7 @@ function handleText(textNode)
 	var v = textNode.nodeValue;
 
 	v = v.replace(/\bThe Trump\b/gi, "The Darklord");
-	v = v.replace(/\bDonald J?\.? Trump\b/gi, "He-Who-Must-Not-Be-Named");
+	v = v.replace(/\bDonald (J)?(\.)? Trump\b/gi, "He-Who-Must-Not-Be-Named");
 	v = v.replace(/\bDonald John Trump\b/gi, "He-Who-Must-Not-Be-Named");
 	v = v.replace(/\bTrump\b/gi, "You-Know-who");
 	v = v.replace(/\bRepublican\b/gi, "Slytherin");
